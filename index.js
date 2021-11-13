aaa9d821382c3ad8c551ccfd79c34a9b75000305
mahmud <picowf@gmail.com>
2021-11-13 23:24:55 +0600

    assainment-done

--- index.js
+++ index.js
@@ -29,13 +29,7 @@ async function run() {
 
     // create a document to insert
 
-    // post some orders
-    app.post("/orders", async (req, res) => {
-      const order = req.body;
-      const result = await orderCollection.insertOne(order);
-      res.json(result);
-    });
-
+    // products
     // post some products
     app.post("/products", async (req, res) => {
       const product = req.body;
@@ -43,17 +37,80 @@ async function run() {
       res.json(result);
     });
 
+    // get some products
+    app.get("/products", async (req, res) => {
+      const cursor = productCollection.find({});
+      const result = await cursor.toArray();
+      res.send(result);
+    });
+
+    // get specific product
+    app.get("/products/:id", async (req, res) => {
+      const id = req.params.id;
+      const query = { _id: ObjectId(id) };
+      const result = await productCollection.findOne(query);
+      res.json(result);
+    });
+
+    // delete one product
+    app.delete("/products/:id", async (req, res) => {
+      const id = req.params.id;
+      const query = { _id: ObjectId(id) };
+      const result = await productCollection.deleteOne(query);
+      res.json(result);
+    });
+
+    // orders
+    // post some orders
+    app.post("/orders", async (req, res) => {
+      const order = req.body;
+      const result = await orderCollection.insertOne(order);
+      res.json(result);
+    });
+
+    // get user order
+    app.get("/order", async (req, res) => {
+      const email = req.query.email;
+      const query = { email: email };
+      const cursor = orderCollection.find(query);
+      const result = await cursor.toArray();
+      res.json(result);
+    });
+
+    //get all orders
+    app.get("/orders", async (req, res) => {
+      const cursor = orderCollection.find({});
+      const result = await cursor.toArray();
+      res.json(result);
+    });
+
+    // status update to database
+    app.put("/orders/update/:id", async (req, res) => {
+      const id = req.params.id;
+      const filter = { _id: ObjectId(id) };
+      const options = { upsert: true };
+      const updateDoc = { $set: { status: "shipped" } };
+      const result = await orderCollection.updateOne(
+        filter,
+        updateDoc,
+        options
+      );
+      res.json(result);
+    });
+
+    // delete one order
+    app.delete("/orders/:id", async (req, res) => {
+      const id = req.params.id;
+      const query = { _id: ObjectId(id) };
+      const result = await orderCollection.deleteOne(query);
+      res.json(result);
+    });
+
+    // users
     // post some users to database
     app.post("/users", async (req, res) => {
       const user = req.body;
       const result = await userCollection.insertOne(user);
-      res.json(result);
-    });
-
-    // post one review
-    app.post("/reviews", async (req, res) => {
-      const review = req.body;
-      const result = await reviewCollection.insertOne(review);
       res.json(result);
     });
 
@@ -76,34 +133,6 @@ async function run() {
       res.json(result);
     });
 
-    // status update to database
-    app.put("/orders/update/:id", async (req, res) => {
-      const id = req.params.id;
-      const filter = { _id: ObjectId(id) };
-      const options = { upsert: true };
-      const updateDoc = { $set: { status: "shipped" } };
-      const result = await orderCollection.updateOne(
-        filter,
-        updateDoc,
-        options
-      );
-      res.json(result);
-    });
-
-    // // update status
-    // app.put("/orders/:id", async (req, res) => {
-    //   const id = req.params.id;
-    //   console.log(id);
-    //   // const query = { _id: ObjectId(id) };
-    //   // const updateDoc = {
-    //   //   $set: {
-    //   //     status: "Approved",
-    //   //   },
-    //   // };
-    //   // const result = await orderCollection.updateOne(query, updateDoc);
-    //   res.json("result");
-    // });
-
     // get special one to make admin
     app.get("/users/:email", async (req, res) => {
       const email = req.params.email;
@@ -116,27 +145,11 @@ async function run() {
       res.json({ admin: isAdmin });
     });
 
-    // get some products
-    app.get("/products", async (req, res) => {
-      const cursor = productCollection.find({});
-      const result = await cursor.toArray();
-      res.send(result);
-    });
-
-    // get user order
-    app.get("/order", async (req, res) => {
-      const email = req.query.email;
-      const query = { email: email };
-      const cursor = orderCollection.find(query);
-      const result = await cursor.toArray();
-      res.json(result);
-    });
-
-    // get specific product
-    app.get("/products/:id", async (req, res) => {
-      const id = req.params.id;
-      const query = { _id: ObjectId(id) };
-      const result = await productCollection.findOne(query);
+    // reviews
+    // post one review
+    app.post("/reviews", async (req, res) => {
+      const review = req.body;
+      const result = await reviewCollection.insertOne(review);
       res.json(result);
     });
 
@@ -145,29 +158,6 @@ async function run() {
       const cursor = reviewCollection.find({});
       const result = await cursor.toArray();
       res.send(result);
-    });
-
-    //get some orders
-    app.get("/orders", async (req, res) => {
-      const cursor = orderCollection.find({});
-      const result = await cursor.toArray();
-      res.json(result);
-    });
-
-    // delete one order
-    app.delete("/orders/:id", async (req, res) => {
-      const id = req.params.id;
-      const query = { _id: ObjectId(id) };
-      const result = await orderCollection.deleteOne(query);
-      res.json(result);
-    });
-
-    // delete one product
-    app.delete("/products/:id", async (req, res) => {
-      const id = req.params.id;
-      const query = { _id: ObjectId(id) };
-      const result = await productCollection.deleteOne(query);
-      res.json(result);
     });
   } finally {
     // await client.close();
